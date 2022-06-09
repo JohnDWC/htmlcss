b3234210ce1769a97dfefdec87819075cb9136f5
John <53600592+JohnDWC@users.noreply.github.com>
2022-06-09 16:17:39 +0930

    Add files via upload

---
+++ htmlcss/HomePageSettings.js
@@ -0,0 +1,8 @@
+function dark() {
+    var container = document.getElementById('container');
+    var password = document.getElementById('password');
+    container.style.backgroundColor='black';
+    container.style.color='gold';
+    password.style.backgroundColor='black';
+    password.style.color='gold';
+}
